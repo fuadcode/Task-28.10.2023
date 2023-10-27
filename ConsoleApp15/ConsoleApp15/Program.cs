64612bc04ffc324c2242965471f0361cc7ad4915
Fuad <fuadgi@code.edu.az>
2023-10-28 00:30:21 +0400

    28.10.2023-homework

---
+++ ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,146 @@
+﻿#region task1
+//Console.WriteLine("4 reqemli eded daxil edin..");
+//int num = int.Parse(Console.ReadLine());
+//string stage = num.ToString();
+//while (true)
+//{
+//    if (stage.Length == 4)
+//    {
+//        Console.WriteLine("7" + stage + "8");
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Daxil etdiyiniz eded yanlishdir..");
+//        stage = Console.ReadLine();
+//    }
+//}
+#endregion
+#region task2
+//Console.WriteLine("Ededi daxil edin:");
+//int numt = int.Parse(Console.ReadLine());
+//string v10 = numt.ToString();
+//while (true)
+//{
+
+//    if (v10.Length == 3)
+//    {
+//        Console.WriteLine(v10 + v10);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Daxil etdiyiniz eded yanlishdir..");
+//        v10 = Console.ReadLine();
+//    }
+//}
+#endregion
+#region task3
+//Console.WriteLine("Ededi daxil edin..");
+//int num3 = int.Parse(Console.ReadLine());
+//string v101 = num3.ToString();
+//while (true)
+//{
+//    if (v101.Length == 5)
+//    {
+//        double result5 = num3 * 0.18;
+//        double result6 = result5 * 0.3;
+//        Console.WriteLine(result6);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Daxil etdiyiniz eded yanlishdir..");
+//        v101 = Console.ReadLine();
+//    }
+//}
+#endregion
+#region task4
+//Console.WriteLine("3 reqemli eded daxil edin");
+//int num4 = int.Parse(Console.ReadLine());
+//string v102 = num4.ToString();
+//while (true)
+//{
+//    if (v102.Length == 3)
+//    {
+//        int numstring1 = num4 * 10 + 7;
+//        int numstring2 = (numstring1 * 7) / 100;
+
+//        Console.WriteLine("Netice: " + numstring2);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Daxil etdiyiniz eded yanlishdir..");
+//        v102 = Console.ReadLine();
+//    }
+//}
+#endregion
+#region task5
+//Console.WriteLine("4 reqemli eded daxil edin");
+//int num5 = int.Parse(Console.ReadLine());
+//string v103 = num5.ToString();
+//while (true)
+//{
+//    if (v103.Length == 4)
+//    {
+//        string result2 = "4" + v103 + "44";
+//        int resultInt = int.Parse(result2);
+//        double lastResult = resultInt * 0.44;
+//        Console.WriteLine(lastResult);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Daxil etdiyiniz eded yanlishdir..");
+//        v103 = Console.ReadLine();
+//    }
+//}
+#endregion
+#region task6
+//Console.WriteLine("4 reqemli eded daxil edin");
+//int num6 = int.Parse(Console.ReadLine());
+//string v104 = num6.ToString();
+//while (true)
+//{
+//    if (v104.Length == 4)
+//    {
+//        double firstsolution = num6 * 0.20;
+//        double Secondsolution = firstsolution * 0.10;
+//        double result = Secondsolution * Secondsolution;
+//        Console.WriteLine(result);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Daxil etdiyiniz eded yanlishdir..");
+//        v104 = Console.ReadLine();
+//    }
+//}
+#endregion
+#region task7
+//int num7 = int.Parse(Console.ReadLine());
+//int num8 = int.Parse(Console.ReadLine());
+//string v105 = num8.ToString();
+//string v106 = num7.ToString();
+//while (true)
+//{
+//    if (v106.Length == 5 && v105.Length == 5)
+//    {
+//        int firsttest = num7 + num8;
+//        string firstsolutiont = firsttest.ToString();
+//        string resultT = "5" + firsttest + "5";
+//        int resultsolution2 = int.Parse(resultT);
+//        double endResult = resultsolution2 * 0.05;
+//        Console.WriteLine(endResult);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Daxil etdiyiniz eded yanlishdir..");
+//        v106 = Console.ReadLine();
+//        v105 = Console.ReadLine();
+//    }
+//}
+#endregion
+
